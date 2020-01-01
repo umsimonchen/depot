7465e4ee5c88573684a5e672529a147df795bf2f
umsimonchen <simontotsuki@gmail.com>
2020-01-01 10:40:58 +0000

    SCSS&cache&asset_select--Test C

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :datetime
+    
+    def datetime
+        @date = Time.now.strftime("%B %e, %Y")
+        @time =Time.now.strftime("%I:%M %p")
+    end
 end
